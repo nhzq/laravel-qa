2ca7b564cd3c57676fbb467dbf053adc3faa9b7d
noorhaziq <code.noorhaziq@gmail.com>
2019-10-31 22:37:43 +0800

    Add votes, answers and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -30,4 +30,17 @@ class Question extends Model
     {
         return $this->created_at->diffForHumans();
     }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answered_id) {
+                return "answered-accepted";
+            }
+
+            return "answered";
+        }
+
+        return "unanswered";
+    }
 }
